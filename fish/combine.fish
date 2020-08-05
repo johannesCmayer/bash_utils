91b8ea658a3abca8cb351be1a194917d3bcd0d55
Johannes C. Mayer <j.c.mayer240@gmail.com>
2020-08-05 09:45:27 +0200

    a

--- fish/combine.fish
+++ fish/combine.fish
@@ -1,24 +1,34 @@
 #!/usr/bin/fish
 
+argparse 'b/border=' 'o/out=' -- $argv
+
+if not set -q _flag_border
+  set _flag_border 2x2
+end
+
+if not set -q _flag_out
+  set _flag_out final.png
+end
+
 if not [ (math (count $argv)%2) -eq 0 ]
   echo "You need to have an even number of arguments"
   exit 1
 end
 
-set td (mktemp -d -t combine_images_XXXX)
+set td (mktemp -d -t combine_images_XXXXXX)
 
 for p in $argv
-  convert -border 6x6 $p $td"/"$p
+  convert -border $_flag_border $p $td"/"$p
 end
 
 for i in (seq 1 2 (count $argv))
-  set f1 $td"/"$argv[$i]
-  set f2 $td"/"$argv[(math "$i + 1")]
-  set out_path $td"/"$i.png
+  set -l f1 $td"/"$argv[$i]
+  set -l f2 $td"/"$argv[(math "$i + 1")]
+  set -l out_path $td"/"$i.png
   convert $f1 $f2 +append $out_path
   set merged $merged $out_path
 end
 
-convert $merged -append final.png
+convert $merged -append $_flag_out
 
 rm -r $td
