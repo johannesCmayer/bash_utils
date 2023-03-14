8b824abbcab7dbd505e6aede5b6030b8887f9dc4
Johannes C. Mayer <j.c.mayer240@gmail.com>
2023-03-14 18:58:23 +0000

    input snatcher

---
+++ python/space_names.py
@@ -0,0 +1,12 @@
+import json
+import argparse
+from pathlib import Path
+
+parser = argparse.parser()
+parser.add_arg('--set-name')
+parser.add_arg('--get-name')
+args = parser.parse()
+
+json.load("")
+
+args.name
--- python/youtube_views.py
+++ python/youtube_views.py
@@ -1,6 +1,6 @@
 import requests
 import re
 
-data = requests.get('https://www.youtube.com/@johannesCmayer/about').content.decode('utf-8')
-match = re.search('"viewCountText":\{"simpleText":"([0-9,]*) views"\}', data)
-print(match.group(1))
+data = requests.get('https://socialblade.com/youtube/channel/UCiNV4S9-xDz_HnYdfY80RHA').content.decode('utf-8')
+match = re.search('\{"simpleText":"([0-9,]*) views"\}', data)
+#print(match.group(1))
